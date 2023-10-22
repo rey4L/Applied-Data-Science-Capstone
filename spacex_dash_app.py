57d2039693fed8380f64338701e1c77bde24ffb9
Rey <reynardetwaroo4@gmail.com>
2023-10-22 15:35:43 -0400

    Uploaded project notebooks

---
+++ spacex_dash_app.py
@@ -0,0 +1,123 @@
+# Import required libraries
+import pandas as pd
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+import plotly.express as px
+
+# Read the airline data into pandas dataframe
+spacex_df = pd.read_csv("spacex_launch_dash.csv")
+max_payload = spacex_df['Payload Mass (kg)'].max()
+min_payload = spacex_df['Payload Mass (kg)'].min()
+
+#print(spacex_df['Launch Site'].unique())
+
+# Create a dash application
+app = dash.Dash(__name__)
+server = app.server
+
+unique_sites = spacex_df['Launch Site'].unique().tolist()
+site_list = []
+site_list.append({'label': 'All Sites', 'value': 'All Sites'})
+
+for site in unique_sites:
+ site_list.append({'label': site, 'value': site})
+
+
+# Create an app layout
+app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
+                                        style={'textAlign': 'center', 'color': 'blue',
+                                               'font-size': 40}),
+
+                                # TASK 1: Add a dropdown list to enable Launch Site selection
+                                # The default select value is for ALL sites
+                                html.P("Select a launch site to filter the data. You can choose to view data from all sites or a specific site."),
+                                dcc.Dropdown(id='site_dropdown',options=site_list,placeholder='Select a Launch Site here', searchable = True , value = 'All Sites'),
+                                html.Br(),
+
+                                # TASK 2: Add a pie chart to show the total successful launches count for all sites
+                                # If a specific launch site was selected, show the Success vs. Failed counts for the site
+                                html.P("The pie chart shows the total successful launches count for all sites or a specific site based on your selection."),        
+                                html.Div(dcc.Graph(id='success-pie-chart')),
+                                html.Br(),
+
+                                html.P("Select a payload range to filter the data. The scatter chart will show the correlation between payload and launch success for the selected range and site."),
+                                html.P("Payload range (Kg):"),
+
+                                # TASK 3: Add a slider to select payload range
+                                dcc.RangeSlider(
+                                    id='payload_slider',
+                                    min=0,
+                                    max=10000,
+                                    step=1000,
+                                    marks = {
+                                            0: '0 kg',
+                                            1000: '1000 kg',
+                                            2000: '2000 kg',
+                                            3000: '3000 kg',
+                                            4000: '4000 kg',
+                                            5000: '5000 kg',
+                                            6000: '6000 kg',
+                                            7000: '7000 kg',
+                                            8000: '8000 kg',
+                                            9000: '9000 kg',
+                                            10000: '10000 kg'
+                                    },
+
+                                    value=[min_payload,max_payload]
+                                ),
+
+                                # TASK 4: Add a scatter chart to show the correlation between payload and launch success
+                                html.Div(dcc.Graph(id='success-payload-scatter-chart')),
+
+                                ])
+# TASK 2:
+# Add a callback function for `site-dropdown` as input, `success-pie-chart` as output
+# Function decorator to specify function input and output
+@app.callback(
+     Output(component_id='success-pie-chart',component_property='figure'),
+     [Input(component_id='site_dropdown',component_property='value')]
+)
+def update_graph(site_dropdown):
+    if (site_dropdown == 'All Sites'):
+        df  = spacex_df[spacex_df['class'] == 1]
+        fig = px.pie(df, names = 'Launch Site',hole=.3,title = 'Percentage of Successful Launches on Various Sites')
+    else:
+        df  = spacex_df.loc[spacex_df['Launch Site'] == site_dropdown]
+        fig = px.pie(df, names = 'class',hole=.3,title = 'Percentage of Successful Launches on Site '+site_dropdown)
+
+    # return the outcomes piechart for a selected site
+    return fig
+
+# TASK 4:
+# Add a callback function for `site-dropdown` and `payload-slider` as inputs, `success-payload-scatter-chart` as output
+@app.callback(
+     Output(component_id='success-payload-scatter-chart',component_property='figure'),
+     [Input(component_id='site_dropdown',component_property='value'),Input(component_id="payload_slider", component_property="value")]
+)
+def update_scattergraph(site_dropdown,payload_slider):
+    if site_dropdown == 'All Sites':
+        low, high = payload_slider
+        df  = spacex_df
+        mask = (df['Payload Mass (kg)'] > low) & (df['Payload Mass (kg)'] < high)
+        fig = px.scatter(
+            df[mask], x="Payload Mass (kg)", y="class",
+            color="Booster Version",
+            size='Payload Mass (kg)',
+            hover_data=['Payload Mass (kg)'])
+    else:
+        low, high = payload_slider
+        df  = spacex_df.loc[spacex_df['Launch Site'] == site_dropdown]
+        mask = (df['Payload Mass (kg)'] > low) & (df['Payload Mass (kg)'] < high)
+        fig = px.scatter(
+            df[mask], x="Payload Mass (kg)", y="class",
+            color="Booster Version",
+            size='Payload Mass (kg)',
+            hover_data=['Payload Mass (kg)'])
+    return fig
+
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(port = 8090, debug=False)
